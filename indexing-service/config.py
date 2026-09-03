caa90f76ea142c2296e9fe8cda992791b5f39b2d
[Manojkumar1710] <kumarakulamanoj7@gmail.com>
2026-09-04 01:32:50 +0530

    updated indexing service

--- indexing-service/config.py
+++ indexing-service/config.py
@@ -1,12 +1,13 @@
+﻿from dotenv import load_dotenv
+load_dotenv()
+
 import os
 
-
 class Settings:
-    EMBEDDING_SERVER_URL: str = os.getenv("EMBEDDING_SERVER_URL", "http://localhost:8003")
+    EMBEDDING_SERVER_URL: str = os.getenv("EMBEDDING_SERVER_URL", "http://localhost:8001")
     QDRANT_URL: str = os.getenv("QDRANT_URL", "http://localhost:6333")
     IMAGES_COLLECTION: str = os.getenv("IMAGES_COLLECTION", "images")
     IMAGE_STORAGE_DIR: str = os.getenv("IMAGE_STORAGE_DIR", "/data/images")
     OCR_LANGUAGES: list[str] = os.getenv("OCR_LANGUAGES", "en").split(",")
-
 
 settings = Settings()
